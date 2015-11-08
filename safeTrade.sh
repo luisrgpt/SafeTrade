f8c41aaf9bfaefac35eaa65d3738879a254dbdda
lribeirogomes <luis.ribeiro.gomes@tecnico.pt>
2015-11-08 22:15:15 +0000

    Fixed file splitting

--- safeTrade.sh
+++ safeTrade.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 function unSafeTrade {
-  rm "${2}"
   touch "${2}"
   for file_name in $(sed -n p "${1}")
   do
@@ -19,9 +18,11 @@ function safeTrade {
 
   for (( ; ; ))
   do
+
     prob=$(expr $(od -vAn -N1 -tu4 < /dev/urandom) % 100)
-    if [ ${prob} -ge 0 ] && [ ${prob} -lt 20 ]
+    if [ ${prob} -ge 0 ] && [ ${prob} -lt 10 ]
     then
+
       if [ $(expr ${total_num}) -le 0 ]
       then
         break
@@ -29,25 +30,27 @@ function safeTrade {
       dir_name=$(mktemp -d --tmpdir="${current_directory}" "$(printf 'X%.0s' $(seq $(expr 4 + $(expr $(od -vAn -N1 -tu4 < /dev/urandom) % 6))))")
       dir_list+=("${dir_name}")
       total_num="$(expr ${total_num} - 4000)"
-    elif [ ${prob} -ge 20 ] && [ ${prob} -lt 50 ]
+
+    elif [ ${prob} -ge 10 ] && [ ${prob} -lt 60 ]
     then
-      if [ $(expr ${total_num}) -le 0 ]
-      then
-        break
-      elif [ $(expr ${total_num}) -le 204 ]
-      then
-        file_size=${total_num}
-      else
-        file_size=$(expr 100 + $(od -vAn -N1 -tu4 < /dev/urandom) % 100)
-      fi
-      file_name=$(mktemp --tmpdir="${current_directory}" "$(printf 'X%.0s' $(seq $(expr 4 + $(expr $(od -vAn -N1 -tu4 < /dev/urandom) % 6))))")
-      dd if=/dev/urandom of=${file_name} bs=1K count=${file_size} status="none";
-      total_num="$(expr ${total_num} - $(expr ${file_size} \* 1000 ) )"
-    elif [ ${prob} -ge 50 ] && [ ${prob} -lt 90 ]
-    then
+
       if [ $(expr ${skip_size}) -ge $(expr ${product_size}) ]
       then
+
+        if [ $(expr ${total_num}) -le 0 ]
+        then
+          break
+        elif [ $(expr ${total_num}) -le 204 ]
+        then
+          file_size=${total_num}
+        else
+          file_size=$(expr 100 + $(od -vAn -N1 -tu4 < /dev/urandom) % 100)
+        fi
+        file_name=$(mktemp --tmpdir="${current_directory}" "$(printf 'X%.0s' $(seq $(expr 4 + $(expr $(od -vAn -N1 -tu4 < /dev/urandom) % 6))))")
+        dd if=/dev/urandom of=${file_name} bs=1K count=${file_size} status="none";
+        total_num="$(expr ${total_num} - $(expr ${file_size} \* 1000 ) )"
         continue
+
       elif [ $(expr ${skip_size}) -ge $(expr ${product_size} - 204000) ]
       then
         file_size=$(expr ${product_size} - ${skip_size})
@@ -60,18 +63,23 @@ function safeTrade {
       skip_size="$(expr ${skip_size} + ${file_size})"
       total_num="$(expr ${total_num} - ${file_size})"
       echo "${file_name}" >> ${2}
-    elif [ ${prob} -ge 90 ] && [ ${prob} -lt 100 ]
+
+    elif [ ${prob} -ge 60 ] && [ ${prob} -lt 100 ]
     then
+
       if [ ${#dir_list[@]} -eq 0 ]
       then
         continue
       fi
       current_directory=("${dir_list[0]}")
       dir_list=("${dir_list[@]:1}")
+      dir_list+=("${current_directory}")
+
     fi
   done
 }
 
+rm "${3}"
 while getopts ":s:u:" opt; do
   case $opt in
     u)
