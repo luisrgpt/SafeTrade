fa0509473f95138603f7f8fdd2503d3b2108898b
lribeirogomes <luis.ribeiro.gomes@tecnico.pt>
2015-11-30 05:37:10 +0000

    Fixed directory problem. Changed conditionals.

--- safeTrade.sh
+++ safeTrade.sh
@@ -1,46 +1,78 @@
 #!/bin/bash
 
+function debugln {
+  if (( ${debug} )); then
+    echo "${1}"
+  fi
+}
+function debug {
+  if (( ${debug} )); then
+    echo -n "${1}"
+  fi
+}
+function randomTemplateGenerator {
+  random_template=$(printf 'X%.0s' $(seq $((4 + $(od -vAn -N1 -tu4 < /dev/urandom) % 6))))
+}
+function randomValueGenerator {
+  random_value=$(($(od -vAn -N3 -tu4 < /dev/urandom) % 200000))
+}
 function createFolder {
-  dir_name=$(mktemp -d --tmpdir="${current_directory}" "$(printf 'X%.0s' $(seq $(expr 4 + $(expr $(od -vAn -N1 -tu4 < /dev/urandom) % 6))))")
+  randomTemplateGenerator
+  dir_name=$(mktemp -d --tmpdir="${current_directory}" ${random_template})
   dir_list+=("${dir_name}")
-  total_num="$(expr ${total_num} - 4000)"
+  total_num=$((${total_num} - 4000))
 }
 function createFile {
-  if [ $(expr ${total_num}) -le 204 ]; then
+  if ((${total_num} <= 204000)); then
     file_size=${total_num}
   else
-    file_size=$(expr 100 + $(od -vAn -N1 -tu4 < /dev/urandom) % 100)
+    randomValueGenerator
+    file_size=${random_value}
   fi
-  file_name=$(mktemp --tmpdir="${current_directory}" "$(printf 'X%.0s' $(seq $(expr 4 + $(expr $(od -vAn -N1 -tu4 < /dev/urandom) % 6))))")
-  dd if=/dev/urandom of="${file_name}" bs=1K count=${file_size} status="none";
-  total_num="$(expr ${total_num} - $(expr ${file_size} \* 1000 ) )"
+  randomTemplateGenerator
+  random_file=$(mktemp --tmpdir=${current_directory} ${random_template})
+  dd if=/dev/urandom of=${random_file} bs=1c count=${file_size} status="none";
+  selected_file=${random_file}
+  file_list+=("${random_file}")
+  total_num=$((${total_num} - ${file_size}))
+  debugln "into ${random_file} (${random_value} bytes)"
 }
 function splitFile {
-  if [ $(expr ${skip_size}) -ge $(expr ${product_size} - 204000) ]; then
-    file_size=$(expr ${product_size} - ${skip_size})
+  if ((${skip_size} >=  ${product_size} - 204000)); then
+    file_size=$((${product_size} - ${skip_size}))
   else
-    file_size=$(expr 100000 + $(od -vAn -N1 -tu4 < /dev/urandom) % 100000)
+    randomValueGenerator
+    file_size=${random_value}
   fi
-  input_file=/dev/zero
-  file_name=$(mktemp --tmpdir=${current_directory} "$(printf 'X%.0s' $(seq $(expr 4 + $(expr $(od -vAn -N1 -tu4 < /dev/urandom) % 6))))")
-  dd if="${1}" of=${file_name} bs=1c count=${file_size} skip=${skip_size} status="none";
-  skip_size="$(expr ${skip_size} + ${file_size})"
-  total_num="$(expr ${total_num} - ${file_size})"
-  echo "${file_name}" | cut -c ${directory_num}-${#file_name} >> ${2}
+  randomTemplateGenerator
+  random_file=$(mktemp --tmpdir=${current_directory} ${random_template})
+  dd if="${1}" of=${random_file} bs=1c count=${file_size} skip=${skip_size} status="none";
+  file_list+=("${random_file}")
+  skip_size=$((${skip_size} + ${file_size}))
+  total_num=$((${total_num} - ${file_size}))
+  echo "${random_file}" | cut -c ${directory_num}-${#random_file} >> ${2}
+  debugln "into ${random_file} (${random_value} bytes)"
 }
 function moveFolder {
-  if [ ${#dir_list[@]} -eq 0 ]; then
+  if ((${#dir_list[@]} == 0)); then
     return
   fi
   current_directory=("${dir_list[0]}")
   dir_list=("${dir_list[@]:1}")
   dir_list+=("${current_directory}")
 }
+function moveFile {
+  if ((${#file_list[@]} == 0)); then
+    return
+  fi
+  selected_file=("${file_list[0]}")
+  file_list=("${file_list[@]:1}")
+  file_list+=("${selected_file}")
+}
 
 function safeTrade {
-
   output_directory="${2}"
-  current_directory="${output_directory}/directory"
+  current_directory=${output_directory}
   directory_num=$((${#current_directory} + 2))
   block_name="${output_directory}/block"
   key_name="${output_directory}/key"
@@ -55,65 +87,109 @@ function safeTrade {
     if [[ -d "${file}" ]]; then
       continue
     fi
+    debugln "Checking file ${file}"
     MD5="${MD5}$(stat -c%s "${file}")\n"
-    MD5="${MD5}$(echo "${file}" | cut -c 2-${#file})\n"
+    MD5="${MD5}$(echo "${file}" | cut -c 1-${#file})\n"
     MD5="${MD5}$(md5sum ${file} | cut -d " " -f1)\n"
     cat "${file}" >> ${block_name}
   done
 
-  gpg --passphrase ${3} -c --cipher-algo aes256 -o "${crypt_name}" "${block_name}"
+  debugln "Encrypting ${block_name} using passphrase ${3} into ${crypt_name}"
+  gpg -q --passphrase ${3} -c --cipher-algo aes256 -o ${crypt_name} ${block_name}
   product_size=$(stat -c%s "${crypt_name}")
 
   skip_size=0
-  total_num=5120000 #00
+  total_num=$((${size} * 1024000)) #00
+  if (($(stat -c%s "${crypt_name}") >= ${total_num} + 204000)); then
+    echo "SafeTrade requires a bigger folder system size"
+    exit 1
+  fi
   dir_list=()
+  file_list=()
 
   for (( ; ; )); do
-
+    debugln "${total_num}"
     prob=$(expr $(od -vAn -N1 -tu4 < /dev/urandom) % 100)
     if [ ${prob} -ge 0 ] && [ ${prob} -lt 10 ]; then
+      debugln "Creating folder"
       createFolder
     elif [ ${prob} -ge 10 ] && [ ${prob} -lt 50 ]; then
+      debug "Spliting file "
       splitFile "${crypt_name}" "${key_name}"
     elif [ ${prob} -ge 50 ] && [ ${prob} -lt 100 ]; then
+      debugln "Moving folder"
       moveFolder
     fi
-    if [ $(expr ${skip_size}) -ge $(expr ${product_size}) ]; then
+    if ((${skip_size} >= ${product_size})); then
+      debugln "Injected all files"
       break
     fi
-
   done
 
   for (( ; ; )); do
-
+    debugln "${total_num}"
     prob=$(expr $(od -vAn -N1 -tu4 < /dev/urandom) % 100)
     if [ ${prob} -ge 0 ] && [ ${prob} -lt 10 ]; then
+      debugln "Creating folder"
       createFolder
     elif [ ${prob} -ge 10 ] && [ ${prob} -lt 50 ]; then
+      debug "Creating file"
       createFile
     elif [ ${prob} -ge 50 ] && [ ${prob} -lt 100 ]; then
+      debugln "Moving folder"
       moveFolder
     fi
-    if [ $(expr ${total_num}) -le 0 ]; then
+    if ((${total_num} <= 0)); then
+      debugln "Completed folder system"
       break
     fi
-
   done
 
   printf "${MD5}" >> ${key_name}
 
-  mv ${key_name} "${key_name}.txt"
+  for (( ; ; )); do
+    debugln "${total_num}"
+    prob=$(expr $(od -vAn -N1 -tu4 < /dev/urandom) % 100)
+    if [ ${prob} -ge 0 ] && [ ${prob} -lt 1 ]; then
+      debugln "Encrypting key with ${selected_file} hash"
+      randomTemplateGenerator
+      random_file=$(mktemp --tmpdir=${current_directory} ${random_template})
+      gpg -q --passphrase "$(md5sum ${selected_file})" -c --cipher-algo aes256 -o ${random_file} ${key_name}
+      break
+    elif [ ${prob} -ge 1 ] && [ ${prob} -lt 100 ]; then
+      moveFile
+      break
+    fi
+  done
 
-  zip ${key_name}.zip "${key_name}.txt"
-  cat "${HOME}/imagem.jpg" "${key_name}.zip" > ${HOME}/imagemcomchave.jpg
+  keykey=${selected_file}
 
-  rm ${block_name} ${crypt_name} "${key_name}.txt"
+  for (( ; ; )); do
+    debugln "${total_num}"
+    prob=$(expr $(od -vAn -N1 -tu4 < /dev/urandom) % 100)
+    if [ ${prob} -ge 0 ] && [ ${prob} -lt 1 ]; then
+      debugln "Moving key key into $(dirname ${selected_file})"
+      mv ${random_file} $(dirname ${selected_file})
+      break
+    elif [ ${prob} -ge 1 ] && [ ${prob} -lt 100 ]; then
+      moveFile
+      break
+    fi
+  done
+
+  keykey="${keykey}\n$(dirname ${selected_file})/$(basename ${random_file})"
+
+  printf "${keykey}" >> "${key_name}.txt"
+  gpg -q --passphrase "$(md5sum ${safe_trade})" -c --cipher-algo aes256 -o "${key_name}1.txt" "${key_name}.txt"
+
+  zip "${key_name}.zip" "${key_name}1.txt"
+  cat ${image} "${key_name}.zip" > ${image}.safe
+
+  rm ${block_name} ${crypt_name} ${key_name} "${key_name}.txt" "${key_name}1.txt" "${key_name}.zip"
 
 }
 function unSafeTrade {
 
-  output_directory="${2}"
-  dir_name="${1}"
   block_name="${output_directory}/deblock"
   crypt_name="${block_name}.gpg"
   mkdir -p ${output_directory}
@@ -123,9 +199,9 @@ function unSafeTrade {
   name=0
   verify=0
   skip_size=0
-  while read -r file_name; do
+  while read -r line; do
     if (( ${verify} )); then
-      if [[ ${md5} = ${file_name} ]]; then
+      if [[ ${md5} = ${line} ]]; then
         echo "${new_file} was extracted with success"
       else
         echo "Error: ${new_file} corrupted"
@@ -134,69 +210,129 @@ function unSafeTrade {
       name=0
       verify=0
     elif (( ${name} )); then
-      new_file="${output_directory}/${file_name}"
-      mv -T "${output_directory}/unamed" ${new_file}
+      debugln "to $(basename ${line})"
+      mkdir -p "${output_directory}/$(dirname ${line})"
+      new_file="${output_directory}/${line}"
+      mv "${output_directory}/unamed" ${new_file}
       md5=$(md5sum ${new_file}  | cut -d " " -f1)
       verify=1
     elif (( ${size} )); then
-      dd if=${block_name} of="${output_directory}/unamed" bs=1c count=${file_name} skip=${skip_size} status="none";
-      skip_size="$(expr ${skip_size} + ${file_name})"
+      debug "Extracting ${line} bytes "
+      dd if=${block_name} of="${output_directory}/unamed" bs=1c count=${line} skip=${skip_size} status="none";
+      skip_size="$(expr ${skip_size} + ${line})"
       name=1
     elif (( ${decrypt} )); then
-      gpg -q --passphrase "${file_name}" -d -o ${block_name} ${crypt_name}
+      debugln "Decrypting ${crypt_name} using passphrase ${line} into ${block_name}"
+      gpg -q --passphrase "${line}" -d -o ${block_name} ${crypt_name}
       size=1
-    elif [[ ${file_name} = "" ]]; then
+    elif [[ ${line} = "" ]]; then
+      debugln "Concatenation completed"
       decrypt=1
     else
-      cat "${dir_name}/${file_name}" >> ${crypt_name}
+      debugln "Joining ${input_directory}/${line} into ${crypt_name}"
+      cat "${input_directory}/${line}" >> ${crypt_name}
     fi
-  done
+  done < "${input_directory}/key"
 
   rm ${block_name} ${crypt_name}
 
 }
 
+safe_trade="${0}"
 reverse=0
-while getopts "ui:o:p:" opt; do
+debug=0
+force=0
+while getopts "hdui:o:p:k:s:" opt; do
   case $opt in
+    h)
+      echo "SafeTrade flags:"
+      echo " -h : Help"
+      echo " -d : Debug mode"
+      echo " -u : Show hidden files"
+      echo " -i : Input file or directory location"
+      echo " -o : Output directory location"
+      echo " -p : Passphrase for encryption (to hide files only)"
+      echo " -k : Key image (to show hidden files only)"
+      echo " -s : Aproximate folder system size in MB (to hide files only)"
+      exit 1
+      ;;
+    d)
+      debug=1
+      ;;
+    f)
+      force=1
+      ;;
     u)
-      #if [[ ${output_file} ]]; then
-      #  echo "Reverse SafeTrade doesn't require output."
-      #  exit 1
-      #fi
       reverse=1
       ;;
     i)
-      input_files=${OPTARG}
+      input_directory="${OPTARG}"
       ;;
     o)
-      #if (( ${reverse}==1 )); then
-      #  echo "Reverse SafeTrade doesn't require output."
-      #  exit 1
-      #fi
-      output_file=${OPTARG}
+      output_directory="${OPTARG}"
       ;;
     p)
-      passphrase=${OPTARG}
+      if (( ${reverse} )); then
+        echo "Showing hidden files doesn't require a passphrase"
+        echo "Exiting SafeTrade"
+        exit 1
+      fi
+      passphrase="${OPTARG}"
+      ;;
+    k)
+      image="${OPTARG}"
+      ;;
+    s)
+      if (( ${reverse} )); then
+        echo "Showing hidden files doesn't require a folder system size"
+        echo "Exiting SafeTrade"
+        exit 1
+      fi
+      size="${OPTARG}"
       ;;
   esac
 done
 
-#if [[ ${input_files} = "" ]]; then
-#  read -p "Input Files: " -r input_files
-#fi
-
-#if [[ ${output_file} = "" ]]; then
-#  read -p "Output Files: " -r output_file
-#fi
+if ! [[ ${input_directory} ]]; then
+  read -p "Input Files [input/]: " -r input_directory
+  input_directory=${input_directory:-input}
+fi
+if ! [[ -d ${input_directory} ]]; then
+  while ! [[ ${input_directory} ]]; do
+    read -p "Input files: " -r input_directory
+  done
+fi
+if ! [[ ${output_directory} ]]; then
+  read -p "Output Directory [output/]: " -r output_directory
+  output_directory=${output_directory:-output}
+fi
+if [[ -d ${output_directory} ]]; then
+  echo "Output directory already exists"
+  echo "Exiting SafeTrade"
+  exit 1
+fi
+if ! (( ${reverse} )) && ! [[ ${passphrase} ]]; then
+  while ! [[ ${passphrase} ]]; do
+    read -p "Passphrase: " -r passphrase
+  done
+fi
+if ! [[ ${image} ]]; then
+  while ! [[ ${image} ]]; do
+    read -p "Image: " -r image
+  done
+fi
+if ! (( ${reverse} )) && ! [[ ${size} ]]; then
+  read -p "Folder system size in MB [5]: " -r size
+  size=${size:-5}
+fi
 
 if (( ${reverse} )); then
   ###################
   # Find Key
   ###################
-  unSafeTrade "${input_files}" "${output_file}"
+  unSafeTrade
 else
-  safeTrade "${input_files}" "${output_file}" "${passphrase}"
+  safeTrade "${input_directory}" "${output_directory}" "${passphrase}"
   ###################
   # Hide Key
   ###################
